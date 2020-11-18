ef11be3987e496a6d5a3ba0d174e61fdca192785
ggg008 <58506208+ggg008@users.noreply.github.com>
2020-11-18 18:41:47 +0900

    ss

---
+++ KIC/200324_node/myJsPractice/coreJS4_07.js
@@ -0,0 +1,9 @@
+"use strict";
+var obj = {
+    vals: [1, 2, 3],
+    logValues: function(v, i) {
+        console.log(this, v, i);
+    }
+};
+obj.logValues(1, 2);
+[4, 5, 6].forEach(obj.logValues);
---
+++ KIC/200324_node/myJsPractice/coreJS4_16.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var addCoffee = function(prevName, name){
+    setTimeout(function(){
+        coffeeMaker.next(prevName ? prevName + ', ' + name : name);
+    }, 500);    
+};
+
+var coffeeGenerator = function* () {
+    var espresso = yield addCoffee('', '에스프레소');
+    console.log(espresso);
+    var americano = yield addCoffee(espresso, '아메리카노');
+    console.log(americano);
+    var mocha = yield addCoffee(americano, '카페오카');
+    console.log(mocha);
+    var latte = yield addCoffee(mocha, '카페라떼');
+    console.log(latte);
+};
+
+var coffeeMaker = coffeeGenerator();
+coffeeMaker.next();
